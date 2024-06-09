48eaf5acfdb3beb1f9bcacd25766ccfa73181db1
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2024-06-09 12:38:58 +0300

    Pregatire Lectia 11

---
+++ lectia-11/practica/script.js
@@ -0,0 +1,50 @@
+/*
+# Task 1
+
+const technologies = ["Javascript", "Python", "CSS", "HTML"];
+
+1. Get container element
+2. Create a cycle for sorting array elements
+3. Create a li tag
+4. Write the text from the array inside the list items
+5. Place li inside ul
+6. Place ul tag with items on the page
+*/
+
+const technologies = ["Javascript", "Python", "CSS", "HTML"];
+
+/*
+# Task 2
+
+Starting from this colors array, create buttons dynamically;
+
+1. Iterate trough the array;
+2. Create a button and set color for the button; (style.backgroundColor, classList.add(""))
+3. Set the text of the button
+4. Add the button to the UI
+*/
+
+const colors = [
+  {
+    label: "Primary Button",
+    color: "#ff0000",
+  },
+  {
+    label: "Click Me",
+    color: "#00ff00",
+  },
+  {
+    label: "Yellow",
+    color: "#fff00f",
+  },
+];
+
+/**
+ * Test Example
+ */
+
+// console.dir(testQuerySelector);
+// console.dir(testQuerySelectorAll[0]);
+// console.dir(testElementById);
+// console.dir(testElementByClass[0]);
+// console.dir(testElementByTag);
